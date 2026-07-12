02f32820c98dda92a191822317ac6a1a294053f0
ANEEKA GUPTA <aneekagupta173@gmail.com>
2026-07-12 13:58:34 +0530

    Update ses3.py

--- ses3.py
+++ ses3.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("MY CHATBOT")
 
-personality = st.selectbox("WHO IS THE PERSONALITY WE ARE TALKING TO TODAY?",[
-    "a common indian man who is frustrated by indian goverment", " a crazy salman khan fan" , "a little boy who beleives the world is a game that only adults play"
+personality = st.sidebar.selectbox("WHO IS THE PERSONALITY WE ARE TALKING TO TODAY?",[
+    "donald trump" , "a poor man who has a bad ego" ,"a common indian man who is frustrated by indian goverment", " a crazy salman khan fan" , "a little boy who beleives the world is a game that only adults play"
 ])
 
 from google import genai
@@ -17,7 +17,8 @@ user_message = st.text_input("START THE CONVERSATION:")
 
 if st.button("SEND"):
     if user_message :
-        ai_inst = f"You are acting as {personality} and stay completely in character and respond in tone necessary to the question asked."
+        ai_inst = {f"You are acting as {personality} and stay completely in character and respond in tone necessary to the question asked.",
+                   f"User message = {user_message}
         with st.spinner(f"calling a {personality}"):  #animaton
             response = client.models._generate_content(
                 model = "gemini-2.5-flash",
